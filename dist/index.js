f769383e855b9ce505d072290576f7b219144603
Alex Gendry <agendry@opentext.com>
2023-10-17 22:24:49 +0200

    WIP

--- dist/index.js
+++ dist/index.js
@@ -38500,10 +38500,10 @@ async function createVulnsByScanProductTable(appId, filterSet = "Security Audito
 async function setJobSummary(app, version) {
     const appId = await appversion.getAppVersionId(app, version);
     await core.summary
-        .addImage('https://github.com/Andhrei/gha-fortify-sast-scan/blob/main/OpenTextBanner.png', 'Fortify by OpenText CyberSecurity')
+        .addImage('https://cdn.asp.events/CLIENT_CloserSt_D86EA381_5056_B739_5482D50A1A831DDD/sites/CSWA-2023/media/libraries/exhibitors/Ezone-cover.png/fit-in/1500x9999/filters:no_upscale()', 'Fortify by OpenText CyberSecurity')
         .addHeading('Fortify AST Results')
         // .addCodeBlock(generateTestResults(), "js")
-        .addTable(await createVulnsByScanProductTable(appId))
+        .addTable(await createVulnsByScanProductTable(appId, 'Information'))
         .addLink('View staging deployment!', 'https://github.com')
         .write();
 }
