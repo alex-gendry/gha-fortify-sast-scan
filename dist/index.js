8a6633ef6a9693c08e62f6211392d7de2eeb0ce5
Alex Gendry <agendry@opentext.com>
2023-10-25 18:03:54 +0200

    fcli v1.3.1

--- dist/index.js
+++ dist/index.js
@@ -42553,6 +42553,7 @@ async function run() {
             }
             core.info(`Copy AppVersion from ${INPUT.ssc_app}:${github.context.payload.head.ref}`);
             INPUT.ssc_source_app = INPUT.ssc_app;
+            console.log(github.context.payload);
             INPUT.ssc_source_version = github.context.payload.head.ref;
         }
         INPUT.ssc_source_version = "1.0-gh-secrets";
